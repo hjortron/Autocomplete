bd3291223ac05b9564911a01aaf87da3da2d30c7
hjortron <percunium@gmail.com>
2015-07-29 00:53:03 +0300

    refactoring

--- AutoComplete/Search/LuceneSearch.cs
+++ AutoComplete/Search/LuceneSearch.cs
@@ -2,32 +2,39 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Lucene.Net;
+using AutoComplete.Properties;
 using Lucene.Net.Analysis.Standard;
 using Lucene.Net.Documents;
 using Lucene.Net.Index;
 using Lucene.Net.QueryParsers;
 using Lucene.Net.Search;
 using Lucene.Net.Store;
-using Version = Lucene.Net.Util.Version;
 
 namespace AutoComplete.Search
 {
-    public static class LuceneSearch
+    public class LuceneSearch
     {
-        private static string _luceneDir =
-        Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "lucene_index");
+        private static readonly string LuceneDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Settings.Default.DICTIONARY_FILE_NAME);
         private static FSDirectory _directoryTemp;
-        private static FSDirectory _directory
+        private static readonly StandardAnalyzer Analyzer = new StandardAnalyzer(Settings.Default.LUCENE_VERSION);
+        private static FSDirectory Directory
         {
             get
             {
-                if (_directoryTemp == null) _directoryTemp = FSDirectory.Open(new DirectoryInfo(_luceneDir));
-                if (IndexWriter.IsLocked(_directoryTemp)) IndexWriter.Unlock(_directoryTemp);
-                var lockFilePath = Path.Combine(_luceneDir, "write.lock");
-                if (File.Exists(lockFilePath)) File.Delete(lockFilePath);
+                if (_directoryTemp == null)
+                {
+                    _directoryTemp = FSDirectory.Open(new DirectoryInfo(LuceneDir));
+                }
+                if (IndexWriter.IsLocked(_directoryTemp))
+                {
+                    IndexWriter.Unlock(_directoryTemp);
+                }
+                var lockFilePath = Path.Combine(LuceneDir, "write.lock");
+                if (File.Exists(lockFilePath))
+                {
+                    File.Delete(lockFilePath);
+                }
+
                 return _directoryTemp;
             }
         }
@@ -36,17 +43,15 @@ namespace AutoComplete.Search
         {
             var doc = new Document();
 
-            doc.Add(new Field("Word", newItem.Word, Field.Store.YES, Field.Index.ANALYZED));
-            doc.Add(new Field("Frequency", newItem.Frequency.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
+            doc.Add(new Field(Settings.Default.WORD_FIELD_NAME, newItem.Word, Field.Store.YES, Field.Index.ANALYZED));
+            doc.Add(new Field(Settings.Default.FREQ_FIELD_NAME, newItem.Frequency.ToString(), Field.Store.YES, Field.Index.NOT_ANALYZED));
 
             writer.AddDocument(doc);
         }
 
         public static void ReadFileToLuceneIndex(string filePath)
-        {
-            // init lucene
-            var analyzer = new StandardAnalyzer(Version.LUCENE_30);
-            using (var writer = new IndexWriter(_directory, analyzer, IndexWriter.MaxFieldLength.UNLIMITED))
+        {                     
+            using (var writer = new IndexWriter(Directory, Analyzer, IndexWriter.MaxFieldLength.UNLIMITED))
             {
                 using (var fileReader = new StreamReader(filePath))
                 {
@@ -54,28 +59,21 @@ namespace AutoComplete.Search
                     while ((stringLine = fileReader.ReadLine()) != null)
                     {
                         var splitedLine = stringLine.Split(' ');
-                        if (splitedLine.Length != 2)
-                        {
-                            continue;
-                        }
                         int freq;
-                        if (!int.TryParse(splitedLine[1], out freq))
+
+                        if (splitedLine.Length == 2 && int.TryParse(splitedLine[Settings.Default.FREQ_POS], out freq))
                         {
-                            continue;
-                        }
-                        var item = new Item(splitedLine[0], freq);
-                        AddToLuceneIndex(item, writer);
+                            var item = new Item(splitedLine[Settings.Default.WORD_POS], freq);
+                            AddToLuceneIndex(item, writer);
+                        }                                                                      
                     }
                 }
-
-                // close handles
-                analyzer.Close();
-                writer.Optimize();
-                writer.Dispose();
+              
+                writer.Optimize();             
             }
         }
 
-        private static Query parseQuery(string searchQuery, QueryParser parser)
+        private static Query ParseQuery(string searchQuery, QueryParser parser)
         {
             Query query;
             try
@@ -84,30 +82,26 @@ namespace AutoComplete.Search
             }
             catch (ParseException)
             {
+                //!!
                 query = parser.Parse(QueryParser.Escape(searchQuery.Trim()));
             }
             return query;
         }
 
         public static IEnumerable<string> Search(string wordForAutocomplete)
-        {
-            // validation
+        {           
             if (string.IsNullOrEmpty(wordForAutocomplete)) return new List<string>();
             wordForAutocomplete += '*';
-            // set up lucene searcher
-            using (var searcher = new IndexSearcher(_directory))
-            {
-                var hits_limit = 10;
-                var analyzer = new StandardAnalyzer(Version.LUCENE_30);
-
-                var parser = new QueryParser(Version.LUCENE_30, "Word", analyzer);
-                var query = parseQuery(wordForAutocomplete, parser);
-                var sort = new Sort(new SortField("Frequency",4, true));
+           
+            using (var searcher = new IndexSearcher(Directory))
+            {                
+                var parser = new QueryParser(Settings.Default.LUCENE_VERSION, Settings.Default.WORD_FIELD_NAME, Analyzer);
+                var query = ParseQuery(wordForAutocomplete, parser);
+                var sortField = new SortField(Settings.Default.FREQ_FIELD_NAME, Settings.Default.LUCENE_INTEGER_TYPE, true);
+                var sort = new Sort(sortField);
                 var filter = new QueryWrapperFilter(query);
-                var hits = searcher.Search(query,filter, hits_limit, sort).ScoreDocs;
-                var results = hits.Select(hit => new Item(searcher.Doc(hit.Doc)).Word).ToArray();
-
-                analyzer.Close();
+                var hits = searcher.Search(query, filter, Settings.Default.HITS_LIMIT, sort).ScoreDocs;
+                var results = hits.Select(hit => searcher.Doc(hit.Doc).GetField(Settings.Default.WORD_FIELD_NAME).StringValue).ToArray();             
                 
                 return results;
             }
