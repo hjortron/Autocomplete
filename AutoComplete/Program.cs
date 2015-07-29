b959119bc72a7fb0571d62fd6c41f19600b59db4
hjortron <percunium@gmail.com>
2015-07-29 20:54:22 +0300

    Console app

--- AutoComplete/Program.cs
+++ AutoComplete/Program.cs
@@ -9,9 +9,19 @@ namespace AutoComplete
     {
       
         static void Main(string[] args)
-        {          
-            Task.Run(() => Server.StartServer());
-            Console.ReadKey();
+        {                   
+            var luceneSearchManager = new LuceneSearch();
+
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+            {
+                var search = Console.ReadLine();
+                var result = luceneSearchManager.Search(search);
+
+                foreach (var word in result)
+                {
+                    Console.WriteLine(word);
+                }
+            }
         }
     }
 }
